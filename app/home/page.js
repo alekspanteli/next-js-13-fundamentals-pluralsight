80345451ef383709549f6e9b8d0e65282fa72cef
alekspanteli <alekspanteli@rambler.ru>
2023-02-19 12:21:40 +0000

    wip

--- app/home/page.js
+++ app/home/page.js
@@ -1,10 +1,14 @@
-import Link from 'next/link';
+import Link from "next/link";
+import Image from "next/image";
+import OurStoryPic from "@/images/home-image-1.jpg";
+import styles from "./home.module.css"
 
 export default function Home() {
-    return (
-      <>
-        <h1>Welcome to home</h1>
-      </>
-    )
-  }
-  
+  return (
+    <>
+      <h1>Welcome to home</h1>
+      {/* <Image className={styles.img} src={OurStoryPic} alt="Test" quality="100" placeholder="blur" /> */}
+      <img src={OurStoryPic} />
+    </>
+  );
+}
