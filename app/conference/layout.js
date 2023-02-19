0450c904d16a263c931dc9006a83ce41caf6068e
alekspanteli <alekspanteli@rambler.ru>
2023-02-19 00:30:06 +0000

    add shared layout

---
+++ app/conference/layout.js
@@ -0,0 +1,12 @@
+
+
+export default function ConferenceLayout({ children }) {
+  return (
+    <>
+      <header>
+        <h1>CONFERENCE LAYOUT HEADER GOES HERE!</h1>
+      </header>
+      <section>{children}</section>
+    </>
+  );
+}
