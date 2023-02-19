855adc0cfcdc5971e71975eb931cd87e9d1a6d86
alekspanteli <alekspanteli@rambler.ru>
2023-02-19 11:27:05 +0000

    add font and images

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,15 @@
 import './globals.css'
+import { Open_Sans } from '@next/font/google';
+
+// If loading a variable font, you don't need to specify the font weight
+const openSans = Open_Sans({
+  subsets: ['latin'],
+  weight: '400',
+})
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={openSans.className}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
