fceaf0a32c4c832602882de23acd4fc113a24a57
alekspanteli <alekspanteli@rambler.ru>
2023-02-19 21:33:30 +0000

    add fetch data

--- app/conference/speakers/page.js
+++ app/conference/speakers/page.js
@@ -1,11 +1,36 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-export default function Home() {
-    return (
-      <main>
-        <h1>Welcome to speakers</h1>
-        <h2><Link href="/conference">back</Link></h2>
-      </main>
-    )
+async function fetchSpeakers() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
   }
-  
+
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await fetchSpeakers();
+  return (
+    <main>
+      <h1>Welcome to speakers</h1>
+      <div style={{ display: "flex", gap: "10px", flexDirection: "column" }}>
+        {data.map(({ id, name, username }) => (
+          <div key={id}>
+            <h5>{name}</h5>
+            <h6>{username}</h6>
+          </div>
+        ))}
+      </div>
+      <div>Last Rendered: {new Date().toLocaleTimeString()}</div>
+      <h2>
+        <Link href="/conference">back</Link>
+      </h2>
+    </main>
+  );
+}
