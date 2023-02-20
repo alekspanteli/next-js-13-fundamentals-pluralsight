31c20ec7f11b9a2404a8ece3c779688243d0929c
alekspanteli <alekspanteli@rambler.ru>
2023-02-20 15:23:06 +0000

    add ssr

--- app/conference/sessions/page.js
+++ app/conference/sessions/page.js
@@ -1,11 +1,36 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-export default function Home() {
-    return (
-      <main>
-        <h1>Welcome to sessions</h1>
-        <h2><Link href="/conference">back</Link></h2>
-      </main>
-    )
+async function fetchSessions() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts", { cache: 'no-store' });
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data");
   }
-  
+
+  return res.json();
+}
+
+export default async function Home() {
+  const data = await fetchSessions();
+  return (
+    <main>
+      <h1>Welcome to sessions</h1>
+      <div style={{ display: "flex", gap: "10px", flexDirection: "column" }}>
+        { data && data.map(({ id, title, body }) => (
+          <div key={id}>
+            <h5>{title}</h5>
+            <h6>{body}</h6>
+          </div>
+        ))}
+      </div>
+      <div>Last Rendered: {new Date().toLocaleTimeString()}</div>
+      <h2>
+        <Link href="/conference">back</Link>
+      </h2>
+    </main>
+  );
+}
